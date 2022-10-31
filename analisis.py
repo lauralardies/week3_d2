1cc983beaa3f782ebe80076a6da7976e83f8a32d
lauralardies <lrodrlar@myuax.com>
2022-10-31 11:07:15 +0100

    nan values por columna

--- analisis.py
+++ analisis.py
@@ -7,3 +7,7 @@ print(data.shape) # 5 rows x 83 columns
 
 # Vamos a ver el nombre de las columnas de nuestro dataframe, sus tipos de datos y los valores None
 print(data.info())
+
+# Creamos un df que cuenta los datos nulos de cada columna
+nan_cols = data.isna().sum()
+print(nan_cols[nan_cols > 0]) # Filtramos aquellas columnas que tengan algún valor nulo
